d658f83bcb64174014e03c7fc48ab41ae926092a
NomadicHippiee <84047650+NomadicHippiee@users.noreply.github.com>
2026-03-23 14:54:02 +0100

    Add append and toString function to linkedList

---
+++ linkedList.js
@@ -0,0 +1,50 @@
+export default class linkedList {
+    constructor() {
+        this.head = null;
+        this.length = 0;
+    }
+
+    append(value) {
+        let newNode = new Node(value, null);
+
+        if (this.head === null) {
+            this.head = newNode;
+        } else {
+            let currentNode = this.head;
+
+            while (currentNode.next !== null) {
+                currentNode = currentNode.next;
+            }
+
+            currentNode.next = newNode;
+        }
+
+        this.length = this.length + 1;
+    }
+
+    toString() {
+        if (this.head === null) {
+            return "";
+        }
+
+        let result = "";
+        let currentNode = this.head;
+
+        while (currentNode !== null) {
+            result +=  "( " + currentNode.value + " ) -> ";
+            currentNode = currentNode.next;
+        }
+
+        result = result + "null";
+
+        return result;
+    }
+}
+
+class Node {
+    constructor(value = null , next = null) {
+        this.value = value;
+        this.next = next;
+
+    }
+}
