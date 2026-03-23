1992ffe0c996771bb106e081f2e38de075d7b2f6
NomadicHippiee <84047650+NomadicHippiee@users.noreply.github.com>
2026-03-23 16:55:13 +0100

    Project test code

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import linkedList from "./linkedList.js";
 
-const list = new linkedList();
+const list = new LinkedList();
 
 list.append("dog");
 list.append("cat");
@@ -9,6 +9,7 @@ list.append("hamster");
 list.append("snake");
 list.append("turtle");
 
-console.log(list.toString());
+console.log(list.toString()); //Should log out ( dog ) -> ( cat ) -> ( parrot ) -> ( hamster ) -> ( snake ) -> ( turtle ) -> null
+
 
 
